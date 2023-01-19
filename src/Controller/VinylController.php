5e47f4e54c976718ebed644d5f996462a9ec65fa
Showaib Haider <Showaibha@gmail.com>
2023-01-19 09:17:03 -0700

    initial commit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,62 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController extends AbstractController {
+
+    #[Route('/home')]
+    public function homePage(){
+    die('Here is the home Page !');
+    }
+
+    #[Route('/info')]
+    public function info(Request $request) : Response
+    {
+        $response = new Response();
+//        $response->setContent(json_encode(
+//            [
+//                'title'=>"json Info from server",
+//                'info' => "Hi! , here is the response from symfony serve"
+//            ]
+//        ));
+//      $name = $request->request->get('name');
+        $file = $request->files->get('file');
+//        $dataName = $data['name'] . " from Server";
+        $path = 'C:\xampp\htdocs\symfonyApp\backEndSymfony\src\Entity';
+        $file->move($path);
+//        $response->setContent(json_encode($dataName));
+
+
+        $python = <<<PYTHON
+    import pandas as pd
+
+    df = pd.DataFrame({$file})
+    result = df.mean()
+    print(result)
+    PYTHON;
+
+        $response->setContent("File uploaded successfully!");
+
+        // for Json data we set the type of header content like that :
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        // Or a predefined website
+        //$response->headers->set('Access-Control-Allow-Origin', 'https://mid.net/');
+
+        // we can set the allowed methods too, if you want
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+
+        return $response;
+    }
+
+}
